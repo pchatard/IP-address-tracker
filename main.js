3988a28200830b8385bcf58c23731a83dcc6c571
pchatard <chatard.pierre@gmail.com>
2020-10-27 12:12:58 +0100

    Cors

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
+const cors = require('cors');
 require("dotenv").config();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
